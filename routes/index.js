ac379f96db9f3d8112c95fecb908699679408990
Roger <rogergomezz.23@gmail.com>
2021-05-11 15:50:16 +0200

    estetica registro hecha

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Coches = require('../models/coches');
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard");
-});
+
 
 router.get("/", async(req, res) => {
     try {
